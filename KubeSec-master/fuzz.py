9f678705561b02f515687246f78d60ae2a84975a
Benjamin Fisk <bfisk1711@gmail.com>
2023-12-01 20:25:38 -0600

    fuzzing test 4

--- KubeSec-master/fuzz.py
+++ KubeSec-master/fuzz.py
@@ -27,18 +27,18 @@ def fuzzValues():
         except Exception as e:
             print(f'Input for checkIfWeirdYAML was invalid.\n Error: {e}\n')
             flag = True
-        # try:
-        #     parser.checkParseError(input)
-        # except Exception as e:
-        #     print(f'Input for checkParseError was invalid.\n Error: {e}\n')
-        #     flag = True
-        # try:
-        #     parser.count_initial_comment_line(input)
-        # except Exception as e:
-        #     print(f'Input for count_initial_comment_line was invalid.\n Error: {e}\n')
-        #     flag = True
-        # if not flag:
-        #     print(f'Input {input} was valid for all functions.\n')
+        try:
+            parser.checkParseError(input)
+        except Exception as e:
+            print(f'Input for checkParseError was invalid.\n Error: {e}\n')
+            flag = True
+        try:
+            parser.count_initial_comment_line(input)
+        except Exception as e:
+            print(f'Input for count_initial_comment_line was invalid.\n Error: {e}\n')
+            flag = True
+        if not flag:
+            print(f'Input {input} was valid for all functions.\n')
 
 if __name__ == '__main__':
     fuzzValues()
