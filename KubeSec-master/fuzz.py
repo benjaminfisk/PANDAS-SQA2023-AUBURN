9cf89b3178f801203906102572259534f6ab9455
Benjamin Fisk <bfisk1711@gmail.com>
2023-12-01 20:45:49 -0600

    add specific inputs

--- KubeSec-master/fuzz.py
+++ KubeSec-master/fuzz.py
@@ -8,41 +8,37 @@ import parser
 
 def fuzzValues():
     with open('fuzzValues.txt', 'w') as f:
-        input_list = [1]
-        for input in input_list:
-            flag = False
-            # print(f'Input: {input}\n')
-            
-            try:
-                parser.checkIfValidHelm(1)
-            except Exception as e:
-                print(f'Input for checkIfValidHelm was invalid.\n Error: {e}\n')
-                f.write(f'Input for checkIfValidHelm was invalid.\n Error: {e}\n')
-                flag = True
-            try:
-                parser.readYAMLAsStr('input')
-            except Exception as e:
-                print(f'Input for checkIfValidK8SYaml was invalid.\n Error: {e}\n')
-                f.write(f'Input for checkIfValidK8SYaml was invalid.\n Error: {e}\n')
-                flag = True
-            try:
-                parser.checkIfWeirdYAML(1)
-            except Exception as e:
-                print(f'Input for checkIfWeirdYAML was invalid.\n Error: {e}\n')
-                f.write(f'Input for checkIfWeirdYAML was invalid.\n Error: {e}\n')
-                flag = True
-            # try:
-            #     parser.checkParseError(input)
-            # except Exception as e:
-            #     print(f'Input for checkParseError was invalid.\n Error: {e}\n')
-            #     flag = True
-            # try:
-            #     parser.count_initial_comment_line(input)
-            # except Exception as e:
-            #     print(f'Input for count_initial_comment_line was invalid.\n Error: {e}\n')
-            #     flag = True
-            # if not flag:
-            #     print(f'Input {input} was valid for all functions.\n')
+        flag = False
+        try:
+            parser.checkIfValidHelm(1)
+        except Exception as e:
+            print(f'Input for checkIfValidHelm was invalid.\n Error: {e}\n')
+            f.write(f'Input for checkIfValidHelm was invalid.\n Error: {e}\n')
+            flag = True
+        try:
+            parser.readYAMLAsStr('input')
+        except Exception as e:
+            print(f'Input for checkIfValidK8SYaml was invalid.\n Error: {e}\n')
+            f.write(f'Input for checkIfValidK8SYaml was invalid.\n Error: {e}\n')
+            flag = True
+        try:
+            parser.checkIfWeirdYAML(1)
+        except Exception as e:
+            print(f'Input for checkIfWeirdYAML was invalid.\n Error: {e}\n')
+            f.write(f'Input for checkIfWeirdYAML was invalid.\n Error: {e}\n')
+            flag = True
+        try:
+            parser.checkParseError('input')
+        except Exception as e:
+            print(f'Input for checkParseError was invalid.\n Error: {e}\n')
+            flag = True
+        try:
+            parser.count_initial_comment_line('input')
+        except Exception as e:
+            print(f'Input for count_initial_comment_line was invalid.\n Error: {e}\n')
+            flag = True
+        if not flag:
+            print(f'Input {input} was valid for all functions.\n')
 
 if __name__ == '__main__':
     fuzzValues()
